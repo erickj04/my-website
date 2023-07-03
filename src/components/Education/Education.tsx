0320706281d4e74963b1c773929b2c786e6780a5
Erick Jovan Muljadi <ejovan2004@gmail.com>
2023-07-03 22:24:13 +0700

    update education section

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import './Skills.css';
+import React, { useState } from 'react';
+import './Education.css';
+import {
+    ListItemButton,
+    Collapse,
+    Typography,
+    Box,
+    List,
+    CircularProgressProps,
+    CircularProgress
+ } from '@mui/material';
+import { 
+    ExpandMore
+} from '@mui/icons-material';
+import { motion } from 'framer-motion';
 
+const Education: React.FC<{}> = () => {
+    const [isFirstOpen, setIsFirstOpen] = useState(false);
+    const [isSecondOpen, setIsSecondOpen] = useState(false);
+    const [isThirdOpen, setIsThirdOpen] = useState(false);
 
-const Skills: React.FC<{}> = () => {
+    const handleClickFirst = () => {
+        setIsFirstOpen(!isFirstOpen);
+    }
+    const handleClickSecond = () => {
+        setIsSecondOpen(!isSecondOpen);
+    }
+    const handleClickThird = () => {
+        setIsThirdOpen(!isThirdOpen);
+    }
+
     return (
         <div className='container'>
             <div className='title'>EDUCATION</div>
-            <div className=''></div>
+            <List className='box-list'>
+                <ListItemButton onClick={handleClickFirst} className='idk'>
+                    <div className='list-title'>
+                        The University of British Columbia
+                        <motion.div animate={{
+                            rotate: isFirstOpen ? 180 : 0
+                        }}>
+                            <ExpandMore />
+                        </motion.div>
+                    </div>
+                    <Collapse in={isFirstOpen} timeout="auto" unmountOnExit className='box-list'>
+                        
+                    </Collapse>
+                </ListItemButton>
+            </List>
         </div>
     )
 }
 
-export default Skills;
+export default Education;
