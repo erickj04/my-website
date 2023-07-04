02534d2d0546f8e1c36c6ba8f12bdf47eec5c5bf
Erick Jovan Muljadi <ejovan2004@gmail.com>
2023-07-04 21:20:19 +0700

    add Experience page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home';
 import ROUTES from './statics/routes';
 import About_me from './pages/About_Me/About_Me';
 import Projects from './pages/Projects/Projects';
+import Experience from './pages/Experience/Experience';
 
 const AppSwitch = () => {
     return (
@@ -10,6 +11,7 @@ const AppSwitch = () => {
         <Route path={ROUTES.HOME} element={<Home />} />
         <Route path={ROUTES.ABOUTME} element={<About_me />} />
         <Route path={ROUTES.PROJECTS} element={<Projects />} />
+        <Route path={ROUTES.EXPERIENCES} element={<Experience />} />
       </Routes>
     );
   }
